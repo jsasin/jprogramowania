8147522d6da7e5daaec322f4a486ff6439fb5668
jsasin <sasinjacek@wp.pl>
2016-02-26 10:39:05 +0100

    Update program6.c

--- jprogramowania/lab1/program6.c
+++ jprogramowania/lab1/program6.c
@@ -1,21 +1,18 @@
 #include<stdio.h>
-//program zawiera warningi, a sprawdzic dzialanie nietypowych konstrukcji skladniowych
-
+  //program zawiera warningi, a sprawdzic dzialanie nietypowych konstrukcji skladniowych
 int main(){
-int a,b,c,d,i,N=10;
-int tab[20];
-//wielokrotne przypisanie tej samej stalej wielu zmiennym
+  int a,b,c,d,i,N=10;
+  int tab[20];
+  //wielokrotne przypisanie tej samej stalej wielu zmiennym
   a = b = c = d = 0;
-//przypisywanie w tablicy, numer indeksu==wartosci przechowywanej w a[indeks]
+  //przypisywanie w tablicy, numer indeksu==wartosci przechowywanej w a[indeks]
   for (i=0; i<N; tab[i++]=i);
-//przypisywanie w tablicy, numer indeksu==wartosci przechowywanej w a[indeks+1]
+  //przypisywanie w tablicy, numer indeksu==wartosci przechowywanej w a[indeks+1]
   for (i=0; i<N; tab[++i]=i);
-
-//mnozenie i *2 do momentu przekroczenia wartosci N
+  //mnozenie i *2 do momentu przekroczenia wartosci N
   i=1; while ((i*=2)<N){
-printf("%d\n",i);
-};
-printf("Wynik po petli %d\n",i);
-
+  printf("%d\n",i);
+  };
+  printf("Wynik po petli %d\n",i);
 return 0;
 }
