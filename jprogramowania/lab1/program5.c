62364e1c4fcef8e4cef47e63a49a989a1cce53f5
jsasin <sasinjacek@wp.pl>
2016-02-26 10:37:29 +0100

    Update program5.c

--- jprogramowania/lab1/program5.c
+++ jprogramowania/lab1/program5.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
 int main(){
-
-int a, b;
-a=2; b=4;
-if ((a=2) & (b=4))   printf(" TAK\n");
-else   printf(" NIE\n");
-
+  int a, b;
+  a=2; b=4;
+  if ((a=2) & (b=4))   printf(" TAK\n");
+  else   printf(" NIE\n");
 return 0;
 }
 
