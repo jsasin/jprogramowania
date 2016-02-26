b4cf1165ea88027ca1696693b68a91679f80f1c0
jsasin <sasinjacek@wp.pl>
2016-02-26 10:36:49 +0100

    Update program2.c

--- jprogramowania/lab1/program2.c
+++ jprogramowania/lab1/program2.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
 int main(){
-char znak;
-
-do{
-znak=getchar();
-printf("%c",znak);
-}
-while(znak!=EOF);
-
+  char znak;
+  do{
+    znak=getchar();
+    printf("%c",znak);
+  }
+  while(znak!=EOF);
 return 0;
 }
